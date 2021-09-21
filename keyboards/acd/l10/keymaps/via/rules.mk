d304d860e6a7d3bd1ce847daf6a7c1d0fb0546aa
Daniel Cai <danielxcai169@gmail.com>
2021-09-21 12:09:34 -0400

    added L10 firmware

---
+++ keyboards/acd/l10/keymaps/via/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = no
